4d23c757011f53d74918b7e7bd35cb170e129e26
zhengyanfeng <zhengyanfeng@haylou.com>
2020-03-14 15:00:13 +0800

    栈实现

--- Stack-Swift.playground/Contents.swift
+++ Stack-Swift.playground/Contents.swift
@@ -1,4 +1,3 @@
-
 example(of: "using a stack") {
     var stack = Stack<Int>()
     
@@ -27,3 +26,56 @@ example(of: "用动态数组初始化一个stack") {
     print(stack)
     stack.pop()
 }
+
+
+/**
+Facebook 一道题目
+ 
+Given an absolute path for a file (Unix-style), simplify it.
+ 
+For example,
+path = "/home/", => "/home"
+path = "/a/./b/../../c/", => "/c"
+ */
+
+func simplifyPath(path: String) -> String {
+    // 判断传入的字符
+    if path.count == 0 {
+        return ""
+    }
+    
+    if path.count == 1 && path == "." {
+        return path
+    }
+    
+    //利用 "/" 拆分原路径 string 成数组
+    let pathArray = path.split(separator: "/")
+    
+    print(pathArray) // 输出 ["a", ".", "b", "..", "..", "c"]
+    
+    // 创建一个 String 类型的栈
+    var pathStack = Stack<String>()
+    
+    for char in pathArray {
+        switch char {
+        case ".":
+            break
+        case "..":
+            pathStack.pop()
+        default:
+            pathStack.push(String(char))
+        }
+    }
+    
+    // 判断 Stack 是否为空
+    if pathStack.isEmpty == true {
+        return "/"
+    }else{
+        let top = pathStack.peek()
+        return "/" + (top ?? "")
+    }
+}
+
+let path_test = "/a/./b/../../c/"
+print(simplifyPath(path: path_test))
+
