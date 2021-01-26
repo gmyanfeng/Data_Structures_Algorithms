a089bda205f22f8fbb3f5cfa4c3151aac8177ccf
fat_panda <1030397448@qq.com>
2021-01-27 02:01:24 +0800

    add stack swift project

---
+++ Data_Structures_Algorithms_Swift/Data_Structures_Algorithms_Swift/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Data_Structures_Algorithms_Swift
+//
+//  Created by iyf on 2021/1/27.
+//
+
+import Foundation
+
+example(of: "using a stack") {
+    var stack = Stack<Int>()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+    print(stack)
+}
