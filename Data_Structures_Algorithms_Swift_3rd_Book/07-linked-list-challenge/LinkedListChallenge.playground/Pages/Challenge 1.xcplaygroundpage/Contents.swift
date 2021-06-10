c139050f0314941da1adc79ca5d93261e90c6255
zhengyanfeng <zhengyanfeng@haylou.com>
2021-06-10 19:46:04 +0800

    增加Linklist challenges 模板

---
+++ Data_Structures_Algorithms_Swift_3rd_Book/07-linked-list-challenge/LinkedListChallenge.playground/Pages/Challenge 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+// Copyright (c) 2019 Razeware LLC
+// For full license & permission details, see LICENSE.markdown.
+/*:
+ # Linked List Challenges
+ ## Challenge 1: Print in reverse
+
+ Create a function that prints the nodes of a linked list in reverse order.
+ */
+
+func printInReverse<T>(_ list: LinkedList<T>) {
+    
+}
+
+var list = LinkedList<Int>()
+list.append(1)
+list.append(2)
+list.append(3)
+print(list)
+printInReverse(list)
+//: [Next Challenge](@next)
